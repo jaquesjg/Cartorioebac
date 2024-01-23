2bcd616d4bad7ff2dbbe08134c131923cabc9534
jaquesjg <157325486+jaquesjg@users.noreply.github.com>
2024-01-23 01:49:53 -0300

    Tela de login
    incluso tela de login com validação de senha

--- Cartório.c
+++ Cartório.c
@@ -117,47 +117,62 @@ int main()//principal função do programa
 {
 	int opcao=0;//Definindo a variável 
 	int laco=1;
+	char senhadigitada[10]="a";
+	int comparacao;
 	
-	for(laco=1; laco=1;)
-	 {
-	 	system("cls");//responsável por limpar a tela
+	printf("\t###Cartório EBAC###\n\n");
+	printf("login de administrador!\n\nDigite a sua senha: ");
+	scanf("%s", senhadigitada);
+	
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
+	{
+	
+	
+		for(laco=1; laco=1;)
+	 	{
+	 		system("cls");//responsável por limpar a tela
 			
-		setlocale(LC_ALL, "portuguese");//definindo linguagem
+			setlocale(LC_ALL, "portuguese");//definindo linguagem
 	
-		printf("Cartório\n\n\tEBAC");
-		printf("\n\nMenu opções");
-		printf("\n1° \tRegistrar nomes");
-		printf("\n2° \tConsultar nomes");
-		printf("\n3° \tDeletar nomes\n\n");
-		printf("4° \tsair do sitema\n\n");
-		printf("Opção:");
+			printf("Cartório\n\n\tEBAC");
+			printf("\n\nMenu opções");
+			printf("\n1° \tRegistrar nomes");
+			printf("\n2° \tConsultar nomes");
+			printf("\n3° \tDeletar nomes\n\n");
+			printf("4° \tsair do sitema\n\n");
+			printf("Opção:");
 	
-		scanf("%d", &opcao);//armazena variáveis do tipo inteiro
+			scanf("%d", &opcao);//armazena variáveis do tipo inteiro
 		
-		system("cls");// responsavel por limpar a tela
+			system("cls");// responsavel por limpar a tela
 		
-		switch (opcao)//inicio da seleção
-		{
-			case 1:
-			registro();
-			break;
+			switch (opcao)//inicio da seleção
+			{
+				case 1:
+				registro();
+				break;
 			
-			case 2:
-			consulta();
-			break;
+				case 2:
+				consulta();
+				break;
 			
-			case 3:
-			deletar();
-			break;
+				case 3:
+				deletar();
+				break;
 			
-			case 4:
-			printf("Obrigado por utilizar o sistema!!\n");
-			return 0;
-			break;
+				case 4:
+				printf("Obrigado por utilizar o sistema!!\n");
+				return 0;
+				break;
 			
-			default:
-			printf("Essa opção não está disponivel\n");
-			system("pause");	
+				default:
+				printf("Essa opção não está disponivel\n");
+				system("pause");	
+			}
 		}
   	}
+  	else
+  	printf("Senha incorreta!!");
 }
